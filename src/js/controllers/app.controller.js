8964f62ef6663b8d2e974a617cd8429c1e1cb991
jimbroski <sam.bogu@gmail.com>
2017-02-18 16:18:04 +0100

    add loading spinner to go_to

--- src/js/controllers/app.controller.js
+++ src/js/controllers/app.controller.js
@@ -24,9 +24,10 @@ class AppController {
 
   // Instance Methods
   go_to(route, params = false){
+    document.querySelector('#root').innerHTML = '<div class="text-center padding-30"><div class="mdl-spinner mdl-js-spinner is-active"></div></div>';
+    componentHandler.upgradeAllRegistered();
     this.current_route = params ? new Routes[route](params) : new Routes[route];
     window.location.hash = `/${this.current_route.controller()}`;
-    componentHandler.upgradeAllRegistered();
   }
 }
 
