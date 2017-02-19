8c433575d3a025aad1411f0f1106a88112a8595b
jimbroski <sam.bogu@gmail.com>
2017-02-19 20:24:11 +0100

    add convert to decimal

--- src/js/models/budget.js
+++ src/js/models/budget.js
@@ -12,6 +12,8 @@ class Budget extends Record {
     this.item_selector = `budget_item_${this.id}`
   };
 
+  // Callbacks
+
   beforeCommit(){
     // doc: framework methods run before save() or update()
     return [
@@ -19,8 +21,8 @@ class Budget extends Record {
       this.validates_numerical([this.amount, this.balance]),
       this.validates_string([this.name]),
       this.validates_max_length_of(20, [this.name]),
-      this.validates([() => (Budget.list_of_icons().indexOf(this.icon) > -1)])
-      // this.convert amount to decimal float
+      this.validates([() => (Budget.list_of_icons().indexOf(this.icon) > -1)]),
+      this.convertToDecimal()
     ];
   };
 
@@ -31,10 +33,18 @@ class Budget extends Record {
     ]
   }
 
+  // Custom Methods
+
   recalculate_balance(){
     if(this.previous_state != undefined){
       this.params.balance = this.balance - (this.previous_state.amount - this.amount);
     };
+    return true;
+  };
+
+  convertToDecimal(){
+    this.params.amount = Number(this.amount).toFixed(2);
+    this.params.balance = Number(this.balance).toFixed(2);
     return true;
   };
 
