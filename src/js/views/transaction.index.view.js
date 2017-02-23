dd80c97adf84b274a1d3fb0b278350bfb0a2cacb
jimbroski <sam.bogu@gmail.com>
2017-02-23 08:23:24 +0100

    small style improvements for transactions

--- src/js/views/transaction.index.view.js
+++ src/js/views/transaction.index.view.js
@@ -8,11 +8,13 @@ class TransactionIndexView {
 
   static templateListItem(transaction){
     return `
-      <div id="${transaction.item_selector}" class="transaction_list_item mdl-list__item mdl-list__item--three-line">
+      <div id="${transaction.item_selector}" class="mdl-list__item mdl-list__item--two-line">
         <span class="mdl-list__item-primary-content">
-          <span>$${transaction.amount}</span><br>
-          <span class="mdl-list__item-sub-title">on ${transaction.name}</span><br>
-          <span>${new Date(transaction.created_at).toDateString()}</span>
+          <span><strong>$${transaction.amount}</strong> on ${transaction.name}</span>
+          <span class="mdl-list__item-sub-title">${new Date(transaction.created_at).toDateString()}</span>
+        </span>
+        <span class="mdl-list__item-secondary-content">
+          <a class="mdl-list__item-secondary-action" href="#"><i class="material-icons">mode-edit</i></a>
         </span>
       </div>`;
   };
